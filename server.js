60bc798f706db522b71de0ee363947f6b6c375dc
Julianag10 <juga2381@colorado.edu>
2025-10-22 12:50:25 -0600

    aligned program container text

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 
 // Home page
 app.get("/", (req, res) => {
-  res.render("home", { title: "Dynamic Test " + new Date().toLocaleTimeString() });
+  res.render("home", { title: "Welcome to Vive" });
 });
 
 // app.get("/", (req, res) => {
