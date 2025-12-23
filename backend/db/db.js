21a4ef397592c7c2c3a05343db286dc0777350cd
Julianag10 <juga2381@colorado.edu>
2025-12-22 20:34:59 -0700

    Backend cleanup: consolidate database logic to pg-promise

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,17 +2,6 @@
 // import { db } from "../db/db.js";
 
 import pgPromise from "pg-promise";
-import path from "path";
-import { fileURLToPath } from "url";
-import dotenv from "dotenv";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({
-  path: path.join(__dirname, "../../.env")
-});
-// dotenv.config();
 
 const pgp = pgPromise();
 
